f9651e64601225add4101251ca60d1d23abe62b3
Fery Wardiyanto <ferywardiyanto@gmail.com>
2021-07-09 02:11:53 +0700

    chore: workaround with firebase
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- website/src/firebase.js
+++ website/src/firebase.js
@@ -1,12 +1,152 @@
 import firebase from 'firebase/app'
-import 'firebase/analytics'
 
-const app = firebase.initializeApp({
+const initialized = {}
+const isDev = import.meta.env.DEV
+const projectId = import.meta.env.VITE_FIREBASE_PROJECTID
+
+const $app = firebase.initializeApp({
   appId: import.meta.env.VITE_FIREBASE_APPID,
   apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECTID,
+  projectId,
 })
 
-export const analytics = import.meta.env.PROD
-  ? firebase.analytics(app)
-  : null
+/**
+ * @param {firebase.app.App} app
+ * @returns {Promise<firebase.analytics.Analytics?>}
+ */
+export const useAnalytics = async (app = $app) => {
+  if (isDev) return null
+
+  if (initialized.analytics) {
+    return initialized.analytics
+  }
+
+  await import('firebase/analytics')
+  initialized.analytics = firebase.analytics(app)
+
+  return initialized.analytics
+}
+
+/**
+ * @param {firebase.app.App} app
+ * @returns {Promise<firebase.auth.Auth>}
+ */
+export const useAuth = async (app = $app) => {
+  if (initialized.auth) {
+    return initialized.auth
+  }
+
+  await import('firebase/auth')
+  const auth = firebase.auth(app)
+
+  if (isDev) {
+    auth.useEmulator('http://localhost:9099')
+  }
+
+  return initialized.auth = auth
+}
+
+/**
+ * @returns {Promise<firebase.User>}
+ */
+export const getCurrentUser = async () => {
+  const auth = await useAuth()
+
+  if (auth.currentUser) {
+    return auth.currentUser
+  }
+
+  return new Promise((resolve, reject) => {
+    const sub = auth.onAuthStateChanged(user => {
+      sub()
+      resolve(user)
+    }, reject)
+  })
+}
+
+/**
+ * @param {firebase.app.App} app
+ * @returns {Promise<firebase.database.Database>}
+ */
+export const useDatabase = async (app = $app) => {
+  if (initialized.database) {
+    return initialized.database
+  }
+
+  await import('firebase/database')
+  const database = firebase.database(app)
+
+  if (isDev) {
+    database.useEmulator('localhost', 9000)
+  }
+
+  return initialized.database = database
+}
+
+/**
+ * @param {firebase.app.App} app
+ * @returns {Promise<firebase.firestore.Firestore>}
+ */
+export const useFirestore = async (app = $app) => {
+  if (initialized.firestore) {
+    return initialized.firestore
+  }
+
+  await import('firebase/firestore')
+  const firestore = firebase.firestore(app)
+
+  if (isDev) {
+    firestore.useEmulator('localhost', 8080)
+  }
+
+  return initialized.firestore = firestore
+}
+
+/**
+ * @param {firebase.app.App} app
+ * @returns {Promise<firebase.functions.Functions>}
+ */
+export const useFunctions = async (app = $app) => {
+  if (initialized.functions) {
+    return initialized.functions
+  }
+
+  await import('firebase/functions')
+  const functions = firebase.functions(app)
+
+  if (isDev) {
+    functions.useEmulator('localhost', 5001)
+  }
+
+  return initialized.functions = functions
+}
+
+/**
+ * @param {firebase.app.App} app
+ * @returns {Promise<firebase.messaging.Messaging>}
+ */
+export const useMessaging = async (app = $app) => {
+  if (initialized.messaging) {
+    return initialized.messaging
+  }
+
+  await import('firebase/messaging')
+  initialized.messaging = firebase.messaging(app)
+
+  return initialized.messaging
+}
+
+/**
+ * @param {firebase.app.App} app
+ * @returns {Promise<firebase.storage.Storage>}
+ */
+export const useStorage = async (app = $app) => {
+  if (initialized.storage) {
+    return initialized.storage
+  }
+
+  await import('firebase/storage')
+  initialized.storage = firebase.storage(app)
+
+  return initialized.storage
+}
--- website/src/main.js
+++ website/src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
-import { createRouter, createWebHistory, isNavigationFailure } from 'vue-router'
-import { analytics } from './firebase'
+import { createRouter, createWebHistory } from 'vue-router'
+import { useAnalytics } from './firebase'
 
 import autoRoutes from 'pages-generated'
 import { setupLayouts } from 'layouts-generated'
@@ -10,39 +10,41 @@ import App from './App.vue'
 import 'windi.css'
 import './main.css'
 
-const routes = autoRoutes.map((route) => {
-  return {
-    ...route,
-    alias: route.path.endsWith('/')
-      ? `${route.path}index.html`
-      : `${route.path}.html`,
-  }
-})
+const app = createApp(App)
+
+app.config.errorHandler = async (error) => {
+  const analytics = await useAnalytics()
+
+  analytics?.logEvent('exception', {
+    description: error.toString(),
+    fatal: true
+  })
+}
 
 const router = createRouter({
-  routes: setupLayouts(routes),
   history: createWebHistory(),
+  routes: setupLayouts(autoRoutes.map((route) => {
+    return {
+      ...route,
+      alias: route.path.endsWith('/')
+        ? `${route.path}index.html`
+        : `${route.path}.html`,
+    }
+  })),
   scrollBehavior (from, to, position) {
     return position || { top: 0 }
   }
 })
 
-router.afterEach((to, from, failure) => {
-  if (isNavigationFailure(failure)) {
-    analytics?.logEvent('exception', {
-      description: failure.message,
-      fatal: false
-    })
-  } else {
-    analytics?.logEvent('page_view', {
-      page_location: to.fullPath,
-      page_path: to.path,
-      page_title: to.meta.title || '',
-    })
-  }
-})
+router.afterEach(async (to) => {
+  const analytics = await useAnalytics()
 
-const app = createApp(App)
+  analytics?.logEvent('page_view', {
+    page_location: to.fullPath,
+    page_path: to.path,
+    page_title: to.meta.title || '',
+  })
+})
 
 app.use(router)
 
