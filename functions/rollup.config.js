a9ec7267c9bb5986eb1bcb5d53602916aa9288a6
Fery Wardiyanto <ferywardiyanto@gmail.com>
2021-08-02 05:30:46 +0700

    chore(build): lint fix
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- functions/rollup.config.js
+++ functions/rollup.config.js
@@ -1,8 +1,9 @@
-import pkg from './package.json'
 import { terser } from 'rollup-plugin-terser'
 import json from '@rollup/plugin-json'
 import commonjs from '@rollup/plugin-commonjs'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
+
+import pkg from './package.json'
 
 const external = Object.keys(Object.assign({}, pkg.dependencies, pkg.devDependencies))
 
--- packages/sendgrid-inbound-parser/rollup.config.js
+++ packages/sendgrid-inbound-parser/rollup.config.js
@@ -12,7 +12,6 @@ export default {
   input: 'index.js',
   output: [
     { file: pkg.main, format: 'cjs', sourcemap: true, exports: 'auto' },
-    { file: pkg.module, format: 'es', sourcemap: true }
   ],
   external
 }
