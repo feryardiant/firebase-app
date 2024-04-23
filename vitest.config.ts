3a7815936207976ae0a97a04c13c0333ef80bb6b
Fery Wardiyanto <ferywardiyanto@gmail.com>
2024-04-23 15:22:46 +0700

    chore(test): enable top-level-await to be able runs wdio
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vitest/config'
+import topLevelAwait from 'vite-plugin-top-level-await'
 
 export default defineConfig({
+  plugins: [
+    topLevelAwait(),
+  ],
   test: {
+    browser: {
+      enabled: true,
+      name: 'chrome',
+      headless: true,
+    },
     globals: true,
     globalSetup: [
       './test/setup.ts',
